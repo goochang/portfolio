9632c71180a1112733e8f7eb762ef82266cee8fb
goochang <cck742@naver.com>
2021-10-28 14:56:01 +0900

    first commit

---
+++ src/components/main/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styles from './Main.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class Main extends Component {
+    render() {
+        return (
+            <div  className={cx('main')}>
+                
+            </div>
+        );
+
+    }
+}
+
+export default Main;
