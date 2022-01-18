c7c97aaaac5ef77c93bc90f4a02da869196d2984
goochang <cck742@naver.com>
2022-01-18 23:18:53 +0900

    .

--- src/components/project/projectContent/ProjectContent.js
+++ src/components/project/projectContent/ProjectContent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './ProjectContent.scss';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
