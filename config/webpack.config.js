36538f5ada821cfa738898f21927a02ec18f6c58
goochang <cck742@naver.com>
2022-01-18 20:54:56 +0900

    .

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -502,22 +502,16 @@ module.exports = function (webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
-                {
-                  importLoaders: 3,
-                  sourceMap: isEnvProduction
-                    ? shouldUseSourceMap
-                    : isEnvDevelopment,
-                }).concat({
-                  loader: require.resolve('sass-loader'),
-                  options: {
-                    additionalData: `@import 'utils';`,
-                    sassOptions: {
-                      includePaths: [paths.appSrc + '/styles'],
-                      sourceMap: isEnvProduction && shouldUseSourceMap,
-                    }
-                  }
-                }),
+              use: getStyleLoaders({
+                importLoaders: 2,
+                sourceMap: isEnvProduction && shouldUseSourceMap
+              }).concat({
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths: [paths.appSrc + '/styles'],
+                  sourceMap: isEnvProduction && shouldUseSourceMap
+                }
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
